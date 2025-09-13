1f176c96380a03376c71429e3686b266838080f5
Tiago Peczenyj <tpeczenyj@weborama.com>
2025-09-13 15:43:29 +0200

    port errors.Join to go 1.19 and 1.18

--- join_go120.go
+++ join_go120.go
@@ -5,7 +5,6 @@ package errors
 import "errors"
 
 // Join returns an error that wraps the given errors.
-// requires minimum go 1.20.
 func Join(errs ...error) error {
 	return errors.Join(errs...)
 }
