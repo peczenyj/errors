a57ced056efb4d14ffe8ec4c7a9043b3f8051edd
Tiago Peczenyj <tiago.peczenyj@gmail.com>
2026-05-29 11:27:45 +0200

    port errors.Join to go 1.19 and 1.18 (#21)
    * port errors.Join to go 1.19 and 1.18
    
    * fix join
    
    * try fix ci

---
+++ join_go190.go
@@ -0,0 +1,68 @@
+//go:build !go1.20
+
+package errors
+
+import (
+	"fmt"
+	"strings"
+)
+
+var (
+	_ error     = joinError(nil)
+	_ unwrapper = joinError(nil)
+)
+
+type joinError []error
+
+func (je joinError) Error() string {
+	if len(je) == 1 {
+		return je[0].Error()
+	}
+
+	var buff strings.Builder
+
+	for index, err := range je {
+		if index > 0 {
+			fmt.Fprintln(&buff)
+		}
+
+		fmt.Fprint(&buff, err.Error())
+	}
+
+	return buff.String()
+}
+
+func (je joinError) Unwrap() []error {
+	return []error(je)
+}
+
+type unwrapper interface {
+	Unwrap() []error
+}
+
+// Join returns an error that wraps the given errors.
+func Join(errs ...error) error {
+	notNilErrs := errs[:0]
+
+	for _, err := range errs {
+		if err != nil {
+			notNilErrs = append(notNilErrs, err)
+		}
+	}
+
+	if len(notNilErrs) == 0 {
+		return nil
+	}
+
+	if len(notNilErrs) == 1 {
+		if _, ok := notNilErrs[0].(unwrapper); ok {
+			return notNilErrs[0]
+		}
+	}
+
+	for i := len(notNilErrs); i < len(errs); i++ {
+		errs[i] = nil
+	}
+
+	return joinError(notNilErrs)
+}
